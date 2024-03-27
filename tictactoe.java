1af9c281ffd11b56ad1a04994e7b6fc4ffae88a1
Harsh Gohil <gohilharsh66002gmail.com>
2024-03-27 17:51:06 -0400

    Use getLable to show the winning info with winning conditions

--- tictactoe.java
+++ tictactoe.java
@@ -10,6 +10,7 @@ class Fdemo extends Frame implements ActionListener{
 	int k=0,x=8,y=28;
 	int a= 0; // O and X.
 	int z=0,z1=0,z2=0,z3=0,z4=0,z5=0,z6=0,z7=0,z8=0,z9=0; 
+	int l = 70;
 	
 	
 	Fdemo(){
@@ -18,8 +19,8 @@ class Fdemo extends Frame implements ActionListener{
 			setVisible(true);
 			setSize(800,600);
 			setLocation(400,100);
-			setBackground(Color.black);
-			setForeground(Color.white);	
+			setBackground(Color.white);
+			setForeground(Color.black);	
 			
 			for(int i=1;i<=3;i++){
 				
@@ -41,7 +42,15 @@ class Fdemo extends Frame implements ActionListener{
 				
 				x=8; 
 				y=y+100;
-			}			
+			}	
+					b1.setSize(150,40);
+					b1 = new Button("New Game");
+					b1.setLocation(500,300);
+					b1.setFont(new Font("",Font.BOLD,20));
+					b1.setForeground(Color.black);
+			
+					add(b1);
+					b1.addActionListener(this);			
 		
 	}
 	
@@ -147,6 +156,186 @@ class Fdemo extends Frame implements ActionListener{
 				b[8].setLabel("X"); z8++; a++;
 			}
 				
+		}
+		// Winning conditions
+		
+		Font f = new Font("",Font.BOLD,20);
+		
+		if(b[0].getLabel()==b[1].getLabel() && b[0].getLabel()==b[2].getLabel()){
+			
+			if(b[0].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[0].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[3].getLabel()==b[4].getLabel() && b[4].getLabel()==b[5].getLabel()){
+			
+			if(b[3].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[3].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[6].getLabel()==b[7].getLabel() && b[6].getLabel()==b[8].getLabel()){
+			
+			if(b[6].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[6].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[0].getLabel()==b[3].getLabel() && b[0].getLabel()==b[6].getLabel()){
+			
+			if(b[0].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[0].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[1].getLabel()==b[4].getLabel() && b[1].getLabel()==b[7].getLabel()){
+			
+			if(b[1].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[1].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[2].getLabel()==b[5].getLabel() && b[5].getLabel()==b[8].getLabel()){
+			
+			if(b[2].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[2].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[0].getLabel()==b[4].getLabel() && b[0].getLabel()==b[8].getLabel()){
+			
+			if(b[0].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[0].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
+		}
+		else if(b[2].getLabel()==b[4].getLabel() && b[2].getLabel()==b[6].getLabel()){
+			
+			if(b[2].getLabel()=="O"){
+				
+				Label p1 = new Label("Player 1 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}
+			
+			if(b[2].getLabel()=="X"){
+				
+				Label p1 = new Label("Player 2 wins");
+				p1.setSize(150,50);
+				p1.setLocation(320,l);
+				p1.setFont(f);
+				add(p1);
+				l+=50;
+			}			
 		}
 	}
 }		
