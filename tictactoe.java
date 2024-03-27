d2b9a9da85203b01da6ab7477ccbf31604ee5278
Harsh Gohil <gohilharsh66002gmail.com>
2024-03-27 17:11:40 -0400

    Create 9 blue buttons and and going to implementing in that frame.

--- tictactoe.java
+++ tictactoe.java
@@ -3,6 +3,11 @@ import java.awt.event.*;
 
 class Fdemo extends Frame implements ActionListener{
 	
+	Button b[] = new Button[9];	
+	int k=0;
+	int x=8;
+	int y=28;
+	
 	
 	Fdemo(){
 			
@@ -11,8 +16,29 @@ class Fdemo extends Frame implements ActionListener{
 			setSize(800,600);
 			setLocation(400,100);
 			setBackground(Color.black);
-			setForeground(Color.white);		
-
+			setForeground(Color.white);	
+			
+			for(int i=1;i<=3;i++){
+				
+				for(int j=1;j<=3;j++){
+					
+					b[k] = new Button();
+					b[k].setSize(100,100);
+					b[k].setLocation(x,y);
+					b[k].setFont(new Font("",Font.BOLD,40));
+					
+					add(b[k]);
+					
+					b[k].addActionListener(this);
+					b[k].setBackground(new Color(255,141,28)); // ORANGE
+					k++;
+					x=x+100;
+					
+				}
+				
+				x=8; 
+				y=y+100;
+			}			
 		
 	}
 	
